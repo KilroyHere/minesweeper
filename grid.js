56fda79e8ba6e63e7ffbaaa900f2350f3fee07f4
Aryan Patel <aroo2001@gmail.com>
2021-08-09 13:40:52 +0530

    Completed Gameplay Mechanics

---
+++ grid.js
@@ -0,0 +1,8 @@
+import Cell from "./cell.js";
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const cellSize = 35;
+let canvasWidth, canvasHeight;
+
+
--- index.js
+++ index.js
@@ -1,44 +1,10 @@
+let gridLength = 20; let grid;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const cellSize = 35;
-const gridLength = 20;
 let canvasWidth, canvasHeight;
 
-class Cell {
-  constructor(posX, posY, size, bombed, state) {
-    this.posX = posX;
-    this.posY = posY;
-    this.size = size;
-    this.endX = posX + size;
-    this.endY = posY + size;
-    this.isBombed = bombed;
-    this.state = state;
-    this.text = this.isBombed ? "💣" : "🌳";
-  }
-
-  setText(text) {
-    this.text = text;
-  }
-
-  drawCell() {
-    //Filled Rectangle  
-    let color = this.isBombed ? "#ffabab" : "#feffd4";
-    ctx.fillStyle = color;
-    ctx.fillRect(this.posX, this.posY, this.size, this.size);
-    //Rectangle Border
-    ctx.fillStyle = "black";
-    ctx.strokeRect(this.posX, this.posY, this.size, this.size);
-    //Filling Text
-    if(this.state === "open" || this.state === "flagged"){
-      ctx.fillStyle = "black";
-      ctx.font = "20px Rubik Mono One";
-      ctx.textAlign = "center";
-      let text = this.state === "flagged" ? "🚩":this.text;
-      ctx.fillText(text, this.posX + 18, this.posY + 25);
-    }
-  }
-};
 
 class Grid {
   constructor(length) {
@@ -92,10 +58,11 @@ class Grid {
     }
   }
 
-  drawGrid() {
+  drawGrid(state="continue") {
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length; j++) {
         let cell = this.cellArray[i][j];
+        cell.state = state === "gameOver" ? "open" : cell.state;
         cell.drawCell();
       }
     }
@@ -104,7 +71,16 @@ class Grid {
   clickCell(x,y){
     let cellRowIndex = Math.floor(x/cellSize);
     let cellColumnIndex = Math.floor(y/cellSize);
-    this.cellArray[cellRowIndex][cellColumnIndex].state = "open";
+    cellRowIndex = cellRowIndex > this.length-1 ? this.length-1 : cellRowIndex;
+    cellColumnIndex = cellColumnIndex > this.length-1 ? this.length-1 : cellColumnIndex;
+    if(this.cellArray[cellRowIndex][cellColumnIndex].state === "closed") {
+      if(this.cellArray[cellRowIndex][cellColumnIndex].isBombed){
+        gameOver();
+        this.drawGrid("gameOver");
+        return;
+      } 
+      this.floodFill(cellRowIndex,cellColumnIndex);
+    }
     this.drawGrid();
   }
 
@@ -113,13 +89,99 @@ class Grid {
     let cellColumnIndex = Math.floor(y/cellSize);
     if(this.cellArray[cellRowIndex][cellColumnIndex].state === "closed") 
       this.cellArray[cellRowIndex][cellColumnIndex].state = "flagged";
+    else if(this.cellArray[cellRowIndex][cellColumnIndex].state === "flagged") 
+      this.cellArray[cellRowIndex][cellColumnIndex].state = "closed";
     this.drawGrid();
+  }
+
+
+  floodFill(r,c){
+
+    if(r >= this.length || r < 0){
+      return;
+    }
+    if(c >= this.length || c < 0){
+      return;
+    }
+    if(this.cellArray[r][c].state === "open" || this.cellArray[r][c].state === "flagged" ){
+      return;
+    } 
+    if(this.cellArray[r][c].text > 0){
+      this.cellArray[r][c].state = "open";
+      return;
+    }
+
+    this.cellArray[r][c].state = "open";
+
+    this.floodFill(r+1,c);
+    this.floodFill(r,c+1);
+    this.floodFill(r-1,c);
+    this.floodFill(r,c-1);
+
   }
 };
 
-const grid = new Grid(gridLength);
-grid.drawGrid();
 
+class Cell {
+  constructor(posX, posY, size, bombed, state) {
+    this.posX = posX;
+    this.posY = posY;
+    this.size = size;
+    this.endX = posX + size;
+    this.endY = posY + size;
+    this.isBombed = bombed;
+    this.state = state;
+    this.text = this.isBombed ? "-2" : "0";
+  }
+
+  setText(text) {
+    this.text = text;
+  }
+
+  drawCell() {
+    //Filled Rectangle  
+    let color = this.state === "open" ? "#ffea63" : "#feffd4";
+    color = this.isBombed && this.state === "open" ? "#ff7a6e" : color;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.posX, this.posY, this.size, this.size);
+
+    //Rectangle Border
+    ctx.fillStyle = "black";
+    ctx.strokeRect(this.posX, this.posY, this.size, this.size);
+
+    //Filling Text
+    let colorArray = ['#00B3E6','#00E680', '#B34D4D','#CC80CC','#1AB399','#CC9999', '#00E680', '#99E6E6', '#6666FF'];
+    if(this.state === "open" || this.state === "flagged"){
+      ctx.fillStyle = colorArray[Math.abs(this.text)];
+      ctx.font = "20px Rubik Mono One";
+      ctx.textAlign = "center";
+      let text;
+      if(this.state === "open" && this.isBombed)
+        text = "💣";
+      else if(this.state === "flagged")
+        text = "🚩";
+      else
+        text = this.text;
+      if(this.text !== 0){
+        ctx.fillText(text, this.posX + 18, this.posY + 25);
+      }
+    }
+  }
+};
+
+function newGame(length){
+  let gameOverText = document.querySelector("#overText");
+  gameOverText.style.display = "none";
+  grid = new Grid(length);
+  grid.drawGrid();
+}
+
+newGame(gridLength);  
+
+function gameOver(){
+  let gameOverText = document.querySelector("#overText");
+  gameOverText.style.display = "block";
+}
 
 canvas.addEventListener("click",function(event){
  const rect = canvas.getBoundingClientRect();
@@ -135,3 +197,11 @@ canvas.addEventListener("contextmenu",function(event){
   const y = event.clientY - rect.top;
   grid.flagCell(x,y);
 });
+
+for (let i = 0; i < 4 ; i++) {
+  document.querySelectorAll("#size")[i].addEventListener("click", function(key) {
+    let length = Number(key.target.innerText.slice(0,2));
+    newGame(length);
+  });
+}
+
