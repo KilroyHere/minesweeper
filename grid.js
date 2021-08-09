ab5ac22766760eb9c76093c2c4f2bfe113a6b397
KilroyHere <66572285+KilroyHere@users.noreply.github.com>
2021-08-09 15:13:18 +0530

    Delete grid.js

--- grid.js
+++
@@ -1,8 +0,0 @@
-import Cell from "./cell.js";
-
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-const cellSize = 35;
-let canvasWidth, canvasHeight;
-
-
